cc9aa0584f3afb90a2bee9060bae480496b5f4d6
nshosain <nazmul.solutionspin@gmail.com>
2023-02-06 17:23:14 +0600

    update UI

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 const API_URL =
   "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple";
 
 function App() {
+  const [questions, setQuestions] = useState({});
   return (
     <div className="container">
       <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
         <h2 className="text-2xl">
-          This is where we're going to have the question.
+          1. This is where we're going to have the question.
         </h2>
       </div>
-      <div className="flex flex-wrap mt-4 justify-around">
-        <button className="bg-white w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-2">
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 1
         </button>
-        <button className="bg-white w-2/5 p-4 text-purple-800 font-semibold rounded shadow mb-2">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 2
         </button>
-        <button className="bg-white w-2/5 p-4 text-purple-800 font-semibold rounded shadow">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 3
         </button>
-        <button className="bg-white w-2/5 p-4 text-purple-800 font-semibold rounded shadow">
+        <button className="bg-white p-4 text-purple-800 font-semibold rounded shadow">
           Answer 4
         </button>
       </div>
